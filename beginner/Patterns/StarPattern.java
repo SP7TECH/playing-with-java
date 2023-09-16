492fe0231e672f9fbacce675d0df9725ea6853f8
SP7TECH <spandey247789@gmail.com>
2023-09-16 14:14:48 +0530

    inverted star pattern

--- beginner/Patterns/StarPattern.java
+++ beginner/Patterns/StarPattern.java
@@ -1,6 +1,6 @@
 package beginner.Patterns;
 
-public class IntroToPatterns {
+public class StarPattern {
     public static void main(String[] args) {
          for(int line = 1; line <= 4; line++) {
             for(int star = 1; star <= line; star++) {
