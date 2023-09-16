ab377953525ef40df4464dd9b9b6cc48a2e3f672
SP7TECH <spandey247789@gmail.com>
2023-09-16 11:47:45 +0530

    using break with do while

---
+++ beginner/Loops/BreakKeyword.java
@@ -0,0 +1,19 @@
+package beginner.Loops;
+import java.util.Scanner;
+
+public class BreakKeyword {
+    public static void main(String args[]) {
+        try(Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter a multiple of 10 to come out of the loop");
+
+            do {
+                int n = sc.nextInt();
+                System.out.println(n);
+
+                if(n % 10 == 0) {
+                    break;
+                }
+            } while(true);
+        }
+    }
+}
