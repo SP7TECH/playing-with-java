d445a31236d83f85d3d2d3e88a58f23e4b368978
SP7TECH <spandey247789@gmail.com>
2023-09-28 01:01:36 +0530

    refactored code for binomial coeff

--- beginner/Functions/BinomialCoefficient.java
+++ beginner/Functions/BinomialCoefficient.java
@@ -13,6 +13,16 @@ public class BinomialCoefficient {
         return factorial;
     }
 
+    public static int calcBinomialCoeff(int n, int r) {
+        int nFactorial = calcFactorial(n);
+        int rFactorial = calcFactorial(r);
+        int diffFactorial = calcFactorial(n - r);
+
+        int binomialCoeff = (nFactorial / (rFactorial * diffFactorial));
+
+        return binomialCoeff;
+    }
+
     public static void main(String[] args) {
         try(Scanner sc = new Scanner(System.in)) {
             System.out.println("Enter n and r respectively to get the Binomial Coefficient");
@@ -20,13 +30,7 @@ public class BinomialCoefficient {
             int n = sc.nextInt();
             int r = sc.nextInt();
 
-            int nFactorial = calcFactorial(n);
-            int rFactorial = calcFactorial(r);
-            int diffFactorial = calcFactorial(n - r);
-
-            int binomialCoeff = (nFactorial / (rFactorial * diffFactorial));
-
-            System.out.println("THe binomial coefficient is " + binomialCoeff);
+            System.out.println("THe binomial coefficient is " + calcBinomialCoeff(n, r));
 
         }
     }
