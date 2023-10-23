cf5a4805d0474dbb3d8d625a6f95830b52c14d96
SP7TECH <spandey247789@gmail.com>
2023-10-23 22:48:42 +0530

    zero one triangle

--- beginner/AdvancedPatterns/ZeroOneTriangle.java
+++ beginner/AdvancedPatterns/ZeroOneTriangle.java
@@ -3,12 +3,16 @@ import java.util.Scanner;
 
 public class ZeroOneTriangle {
     public static void printPattern(int n) {
-        int count = 1;
+        // int count = 1;
         for(int i = 1; i <= n; i++) {
             for(int j = 1; j <= i; j++) {
-                System.out.print(count + " ");
-                if(count == 1) count = 0;
-                else count = 1;
+                if((i + j) % 2 == 0) {
+                    System.out.print("1 ");
+                } else {
+                    System.out.print("0 ");
+                }
+                // if(count == 1) count = 0;
+                // else count = 1;
             }
 
             System.out.println();
@@ -17,7 +21,7 @@ public class ZeroOneTriangle {
 
     public static void main(String[] args) {
         try(Scanner sc = new Scanner(System.in)) {
-            System.out.println("Enter the number of stars to be printed");
+            System.out.println("Enter the number of lines to be printed");
 
             int n = sc.nextInt();
             printPattern(n);
