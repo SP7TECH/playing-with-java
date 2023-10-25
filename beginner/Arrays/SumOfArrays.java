0ea15a375809ba760e0eaa1c84727840491a4516
SP7TECH <spandey247789@gmail.com>
2023-10-25 10:18:34 +0530

    sum of elements in arrays

---
+++ beginner/Arrays/SumOfArrays.java
@@ -0,0 +1,32 @@
+package beginner.Arrays;
+import java.util.Scanner;
+
+public class SumOfArrays {
+    public static int printSum(int arr[]) {
+        int sum = 0;
+
+        for(int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        try(Scanner sc = new Scanner(System.in)) {
+
+            System.out.println("Enter the length of the array");
+            int n = sc.nextInt();
+
+            int marks[] = new int[n];
+
+            System.out.println("Enter " + n + " numbers to get their sum");
+            for(int i = 0; i < n; i++) {
+                marks[i] = sc.nextInt();
+            }
+
+            System.out.println(printSum(marks));
+
+        }
+    }
+}
