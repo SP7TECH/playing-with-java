061ce4cbaa4445f2516e465fc9d35aab183f0821
SP7TECH <spandey247789@gmail.com>
2023-11-06 20:20:00 +0530

    sub arrays

---
+++ beginner/Arrays/SubArrays.java
@@ -0,0 +1,24 @@
+package beginner.Arrays;
+
+public class SubArrays {
+    public static void printSubArrays(int arr[]) {
+        for(int i = 0; i < arr.length; i++) {
+            int start = i;
+            for(int j = i + 1; j < arr.length; j++) {
+                int end = j;
+                for(int k = start; k < end; k++) {
+                    System.out.print(arr[k] + " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+     
+        int arr[] = {2, 4, 6, 8, 10};
+        printSubArrays(arr);
+
+    }
+}
