eba083b9be105de6820859a44e3d64648708ad5e
SP7TECH <spandey247789@gmail.com>
2023-11-30 08:32:47 +0530

    trapping rainwater

---
+++ beginner/Arrays/TrappingRainwater.java
@@ -0,0 +1,12 @@
+package beginner.Arrays;
+
+public class TrappingRainwater {
+
+    public static int trappingRainwater(int n) {
+        return n;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
