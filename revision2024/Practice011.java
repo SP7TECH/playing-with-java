b7a8bf31b0904d1cf8754426fa7bc28ee07d75c3
SP7TECH <spandey247789@gmail.com>
2024-01-24 09:13:37 +0530

    bubble sort

---
+++ revision2024/Practice011.java
@@ -0,0 +1,31 @@
+package revision2024;
+
+public class Practice011 {
+    public static void bubbleSort(int arr[]) {
+        int n = arr.length;
+
+        for(int i = 0; i < n - 1; i++) {
+            int swaps = 0;
+            for(int j = 0; j < n - 1 - i; j++) {
+                if(arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    ++swaps;
+                }
+
+                if(j == 1 && swaps == 0) return;
+            }
+        }
+
+        for(int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    
+    public static void main(String[] args) {
+        int arr[] = {5, 4, 1, 3, 2};
+        bubbleSort(arr);
+    }
+}
