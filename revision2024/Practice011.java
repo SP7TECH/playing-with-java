5d2876ca4b4975f755ce332c1d3d7a2f8719f7c9
Sumit Pandey <spandey247789@gmail.com>
2024-01-26 11:22:22 +0530

    insertion sort

--- revision2024/Practice011.java
+++ revision2024/Practice011.java
@@ -1,6 +1,16 @@
 package revision2024;
 
 public class Practice011 {
+    public static void printArr(int arr[]) {
+        int n = arr.length;
+        if(n == 0) System.out.println("Empty Array");
+        
+        for(int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
     public static void bubbleSort(int arr[]) {
         int n = arr.length;
         if(n == 0) return;
@@ -20,10 +30,7 @@ public class Practice011 {
         }
 
         System.out.println("Bubble Sort");
-        for(int i = 0; i < n; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
+        printArr(arr);
     }
 
     public static void selectionSort(int arr[]) {
@@ -44,15 +51,33 @@ public class Practice011 {
         }
         
         System.out.println("Selection Sort");
-        for(int i = 0; i < n; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
+        printArr(arr);
     }
     
+    public static void insertionSort(int arr[]) {
+        int n = arr.length;
+        if(n == 0) return;
+
+        for(int i = 1; i < n; i++) {
+            int current = arr[i];
+            int prevIndex = i - 1;
+
+            while (prevIndex >= 0 && arr[prevIndex] > current) {
+                arr[prevIndex + 1] = arr[prevIndex];
+                prevIndex--;
+            }
+
+            arr[prevIndex + 1] = current;
+        }
+
+        System.out.println("Insertion Sort");
+        printArr(arr);
+    }
+
     public static void main(String[] args) {
         int arr[] = {5, 4, 1, 3, 2};
         bubbleSort(arr);
         selectionSort(arr);
+        insertionSort(arr);
     }
 }
