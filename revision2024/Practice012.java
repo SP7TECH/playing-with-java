c707c5d748c7a347566718d5267ddd6be05e36c6
Sumit Pandey <spandey247789@gmail.com>
2024-01-31 08:41:56 +0530

    diagonal sum

--- revision2024/Practice012.java
+++ revision2024/Practice012.java
@@ -27,6 +27,27 @@ public class Practice012 {
         }
     }
 
+    public static void diagonalSum(int arr[][]) {
+        int sum = 0;
+        int rows = arr.length;
+        int cols = arr[0].length;
+
+        if(rows != cols) {
+            System.out.println("A square matrix needed");
+            return;
+        }
+
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                if(i == j) {
+                    sum += arr[i][j];
+                }
+            }
+        }
+
+        System.out.println("Diagonal Sum: " + sum);
+    }
+
     public static void main(String[] args) {
         try(Scanner sc = new Scanner(System.in)) {
             System.out.println("Enter the number of rows");
@@ -49,6 +70,8 @@ public class Practice012 {
             System.out.println("Enter an element to search in the array");
             int toSearch = sc.nextInt();
             searchInArray(arr, toSearch);
+
+            diagonalSum(arr);
         }
     }
 }
