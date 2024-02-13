524785734961bf3635778d7b0e2b94144e88b697
Sumit Pandey <spandey247789@gmail.com>
2024-02-13 08:15:22 +0530

    sorting algo

--- revision2024/Practice013.java
+++ revision2024/Practice013.java
@@ -95,12 +95,51 @@ public class Practice013 {
         return arr;
     }
 
+    public static int[] selectionSort(int arr[]) {
+        int n = arr.length - 1;
+
+        for(int i = 0; i < n; i++) {
+            int minIndex = i;
+
+            for(int j = i + 1; j < n; j++) {
+                if(arr[minIndex] > arr[j]) {
+                    minIndex = j;
+                }
+            }
+
+            // swapping outside of the nested loop
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+
+        return arr;
+    }
+
     public static void printArray(int arr[]) {
         for(int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
 
         System.out.println();
+    }
+
+    public static int[] insertionSort(int arr[]) {
+        int n = arr.length;
+        
+        for(int i = 1; i < n; i++) {
+            int currentElement = arr[i];
+            int prevIndex = i - 1;
+
+            while(prevIndex >= 0 && arr[prevIndex] > currentElement) {
+                arr[prevIndex + 1] = arr[prevIndex];
+                prevIndex--;
+            }
+
+            arr[prevIndex + 1] = currentElement;
+        }
+        
+        return arr;
     }
     
     public static void main(String[] args) {
@@ -119,7 +158,14 @@ public class Practice013 {
             System.out.println("Element found at index: " + binarySearch(arr, elementToSearch));
 
             int arr2[] = {5, 4, 1, 3, 2};
+            System.out.print("Bubble Sort ------> ");
             printArray(bubbleSort(arr2));
+            
+            System.out.print("Selection Sort ------> ");
+            printArray(bubbleSort(arr2));
+
+            System.out.print("Insertion Sort ------> ");
+            printArray(insertionSort(arr2));
         }
     }
 }
