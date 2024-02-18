f5ba2959521c181fbfdee8f13d3ff0135742d851
Sumit Pandey <spandey247789@gmail.com>
2024-02-18 12:27:28 +0530

    string basics

---
+++ basics/Strings/Intro.java
@@ -0,0 +1,25 @@
+package basics.Strings;
+import java.util.Scanner;
+
+public class Intro {
+    public static void printArray(char arr[]) {
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        try(Scanner sc = new Scanner(System.in)) {
+            char stringArray[] = {'T', 'o', 'n', 'y'};
+            String name = "Tony";
+            String lastName = new String("Stark");
+
+            printArray(stringArray);
+            System.out.println(name);
+            System.out.println(lastName);
+
+            // Input for Strings
+        }
+    }
+}
