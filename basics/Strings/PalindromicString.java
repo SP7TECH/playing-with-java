d3b88ca1839a4f1d22502679c2e4f5d364352503
Sumit Pandey <spandey247789@gmail.com>
2024-02-20 23:34:57 +0530

    palindromic string

---
+++ basics/Strings/PalindromicString.java
@@ -0,0 +1,10 @@
+package basics.Strings;
+import java.util.Scanner;
+
+public class PalindromicString {
+    public static void main(String[] args) {
+        try(Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter a string to check if it is palindromic or not: ");
+        }
+    }
+}
