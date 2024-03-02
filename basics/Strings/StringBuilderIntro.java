3b7b049a997dd5af6078f986b6f3c2ea5865c69e
Sumit Pandey <spandey247789@gmail.com>
2024-03-02 10:51:42 +0530

    string builder append method

--- basics/Strings/StringBuilderIntro.java
+++ basics/Strings/StringBuilderIntro.java
@@ -2,6 +2,15 @@ package basics.Strings;
 
 public class StringBuilderIntro {
     public static void main(String[] args) {
+        StringBuilder name = new StringBuilder("Tony Stark");
+        System.out.println(name);
         
+        
+        StringBuilder alphabets = new StringBuilder("");
+        System.out.println(alphabets);
+        for(char ch = 'a'; ch <= 'z'; ch++) {
+            alphabets.append(ch);
+        } 
+        System.out.println(alphabets);
     }
 }
