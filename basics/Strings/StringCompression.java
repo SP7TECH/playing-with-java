0dc49e1ef998294ed33f07865bd3fe666cafcb30
Sumit Pandey <spandey247789@gmail.com>
2024-03-02 12:09:48 +0530

    another compress methods

--- basics/Strings/StringCompression.java
+++ basics/Strings/StringCompression.java
@@ -28,12 +28,32 @@ public class StringCompression {
         return compressedStr.toString();
     }
 
+    public static String compress(String str) {
+        String compressedStr = "";
+
+        for(int i = 0; i < str.length(); i++) {
+            Integer count = 1;
+            while(i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)) {
+                count++;
+                i++;
+            }
+
+            compressedStr += str.charAt(i);
+            if(count > 1) {
+                compressedStr += count.toString();
+            }
+        }
+
+        return compressedStr;
+    }
+ 
     public static void main(String[] args) {
         try(Scanner sc = new Scanner(System.in)) {
             System.out.println("Enter a string: ");
             
             String str = sc.nextLine();
             System.out.println("Compressed String: " + compressString(str));
+            System.out.println("Compressed String: " + compress(str));
         }
     }
     
