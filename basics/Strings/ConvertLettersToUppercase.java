990a7700f63707e3f1617024448e8091ebc8818a
Sumit Pandey <spandey247789@gmail.com>
2024-03-02 11:39:51 +0530

    title case string using string builder

--- basics/Strings/ConvertLettersToUppercase.java
+++ basics/Strings/ConvertLettersToUppercase.java
@@ -2,12 +2,31 @@ package basics.Strings;
 import java.util.Scanner;
 
 public class ConvertLettersToUppercase {
+    public static String toUpperCase(String str) {
+        StringBuilder upperCaseStr = new StringBuilder("");
+    
+        char ch = Character.toUpperCase(str.charAt(0));
+        upperCaseStr.append(ch);
+
+        for(int i = 1; i < str.length(); i++) {
+            if(str.charAt(i) == ' ') {
+                upperCaseStr.append(str.charAt(i));
+                i++;
+                upperCaseStr.append(Character.toUpperCase(str.charAt(i)));
+            } else {
+                upperCaseStr.append(str.charAt(i));
+            }
+        }
+
+        return upperCaseStr.toString();
+    }
+    
     public static void main(String[] args) {
         try(Scanner sc = new Scanner(System.in)) {
             System.out.println("Enter a string: ");
 
             String str = sc.nextLine();
-            System.out.println(str);
+            System.out.println("Transformed to uppercase: " + toUpperCase(str));
         }
     }
     
