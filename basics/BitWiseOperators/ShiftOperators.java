7a9dfbea82387813c6ca5e9710b246591bd7d652
Sumit Pandey <spandey247789@gmail.com>
2024-03-11 23:49:16 +0530

    binary left shift operator

--- basics/BitWiseOperators/ShiftOperators.java
+++ basics/BitWiseOperators/ShiftOperators.java
@@ -1,5 +1,20 @@
 package basics.BitWiseOperators;
+import java.util.Scanner;
 
 public class ShiftOperators {
-    
+    public static int leftShift(int a, int b) {
+        return (int)(a * Math.pow(b, 2));
+    }
+
+    public static void main(String[] args) {
+        try(Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter two numbers: ");
+
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            System.out.println("a << b = " + (a << b));
+            System.out.println("With custom methods: a << b = " + leftShift(a, b));
+        }
+    }
 }
