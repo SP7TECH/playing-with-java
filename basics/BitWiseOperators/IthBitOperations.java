945ed3829129c7b2b2b99a5453d66379e8c26251
Sumit Pandey <spandey247789@gmail.com>
2024-03-14 07:48:41 +0530

    get ith bit

---
+++ basics/BitWiseOperators/IthBitOperations.java
@@ -0,0 +1,25 @@
+package basics.BitWiseOperators;
+import java.util.Scanner;
+
+public class IthBitOperations {
+    public static int getIthBit(int n, int i) {
+        int bitMask = 1 << i;
+        if((n & bitMask) == 0) {
+            return 0;
+        }
+
+        return 1;
+    }
+    
+    public static void main(String[] args) {
+        try(Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter a number: ");
+            int n = sc.nextInt();
+
+            System.out.println("Enter the position to check if the bit is 1 or 0: ");
+            int i = sc.nextInt();
+
+            System.out.println(getIthBit(n, i));
+        }
+    }
+}
