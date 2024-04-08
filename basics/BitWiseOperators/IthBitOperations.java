264be8ba610ea4ed25752db2b2aca8d992021839
Sumit Pandey <spandey247789@gmail.com>
2024-04-08 09:28:37 +0530

    update bit

--- basics/BitWiseOperators/IthBitOperations.java
+++ basics/BitWiseOperators/IthBitOperations.java
@@ -21,8 +21,12 @@ public class IthBitOperations {
         return n & bitMask;
     }
 
-    public static int updateBit(int n, int i) {
-        return n;
+    public static int updateBit(int n, int i, int newBit) {
+        if(newBit == 0) {
+            return clearIthBit(n, i);
+        } else {
+            return setIthBit(n, i);
+        }
     }
     
     public static void main(String[] args) {
@@ -42,6 +46,11 @@ public class IthBitOperations {
             System.out.println("Enter the position to clear the bit i.e. make it 0: ");
             int pos = sc.nextInt();
             System.out.println(clearIthBit(n, pos));
+
+            System.out.println("Enter the position and new bit: ");
+            int newPos = sc.nextInt();
+            int newBit = sc.nextInt();
+            System.out.println(updateBit(n, newPos, newBit));
         }
     }
 }
