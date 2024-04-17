d169dd13ddf1b7b6062ac3b758ae3d88ccbb76cc
Sumit Pandey <spandey247789@gmail.com>
2024-04-17 09:06:04 +0530

    diagonal sum matrix

---
+++ leetcode/Matrix/DiagonalSum.java
@@ -0,0 +1,25 @@
+package leetcode.Matrix;
+
+public class DiagonalSum {
+    public static int diagonalSumBruteForce(int[][] mat) {
+        int sum = 0;
+        for(int i = 0; i < mat.length; i++) {
+            for(int j = 0; j < mat[0].length; j++) {
+                if(i == j) {
+                    sum += mat[i][j];
+                }
+
+                if(i + j == mat.length - 1 && i != j) {
+                    sum += mat[i][j];
+                }
+            }
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int matrix[][] = {{1,2,3}, {4,5,6}, {7,8,9}};
+        System.out.println(diagonalSumBruteForce(matrix));
+    }
+}
