d787b423899dacd160a6014d748c6aee012626b9
Sumit Pandey <spandey247789@gmail.com>
2024-04-29 22:55:41 +0530

    parametrized constructor

--- basics/OOPS/Constructors.java
+++ basics/OOPS/Constructors.java
@@ -4,6 +4,9 @@ public class Constructors {
     public static void main(String[] args) {
         Student s1 = new Student("Shradha");
         System.out.println(s1.name);
+
+        Student s2 = new Student(26);
+        System.out.println(s2.age);
     }
 }
 
@@ -11,7 +14,15 @@ class Student {
     String name;
     int age;
 
+    Student() {
+        System.out.println("Constructor called");
+    }
+
     Student(String name) {
         this.name = name;
+    }
+
+    Student(int age) {
+        this.age = age;
     }
 }
