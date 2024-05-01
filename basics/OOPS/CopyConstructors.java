7b7f412df167d52c515bda8e6cabb5215c37d0dc
Sumit Pandey <spandey247789@gmail.com>
2024-05-01 21:23:36 +0530

    shallow copy constructor

--- basics/OOPS/CopyConstructors.java
+++ basics/OOPS/CopyConstructors.java
@@ -6,12 +6,18 @@ public class CopyConstructors {
         s1.name = "Shradha";
         s1.roll = 456;
         s1.password = "abcd";
+        s1.marks[0] = 100;
+        s1.marks[1] = 90;
+        s1.marks[2] = 80;
 
         Scholar s2 = new Scholar(s1);
         s2.password = "efgh";
-        System.out.println(s2.name);
-        System.out.println(s2.roll);
-        System.out.println(s2.password);
+        System.out.println("Name: " + s2.name + ", Roll: " + s2.roll + ", Password: " + s2.password);
+
+        s1.marks[2] = 95;
+        for(int i = 0; i < s1.marks.length; i++) {
+            System.out.print(s2.marks[i] + ", ");
+        }
     }    
 }
 
@@ -22,11 +28,13 @@ class Scholar {
     int marks[];
 
     Scholar() {
+        marks = new int[3];
         System.out.println("constructor called");
     }
 
     Scholar(Scholar s1) {
         this.name = s1.name;
         this.roll = s1.roll;
+        marks = s1.marks;
     }
 }
