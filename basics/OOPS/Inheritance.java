4aaf926b530d620b914ea4f76f41bb11df35b8e4
Sumit Pandey <spandey247789@gmail.com>
2024-05-03 09:14:42 +0530

    Single level inheritance

--- basics/OOPS/Inheritance.java
+++ basics/OOPS/Inheritance.java
@@ -2,6 +2,27 @@ package basics.OOPS;
 
 public class Inheritance {
     public static void main(String[] args) {
-        System.out.println("Inheritance");
+        Fish salmon = new Fish();
+        salmon.eats();
+    }
+}
+
+class Animal {
+    String color;
+
+    void eats() {
+        System.out.println("eats");
+    }
+
+    void breathes() {
+        System.out.println("breathes");
+    }
+}
+
+class Fish extends Animal {
+    int fins;
+
+    void swim() {
+        System.out.println("swims in water");
     }
 }
