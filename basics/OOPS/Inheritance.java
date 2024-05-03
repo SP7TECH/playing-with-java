e9716e0d7b1709e68bd93e5cef61f96188f0403f
Sumit Pandey <spandey247789@gmail.com>
2024-05-03 09:19:37 +0530

    multi level inheritance

--- basics/OOPS/Inheritance.java
+++ basics/OOPS/Inheritance.java
@@ -2,8 +2,18 @@ package basics.OOPS;
 
 public class Inheritance {
     public static void main(String[] args) {
+        // Single level
         Fish salmon = new Fish();
         salmon.eats();
+
+        // Multi level
+        Dog Gigi = new Dog();
+        Gigi.eats();
+        Gigi.breed = "Rottweiler";
+        System.out.println(Gigi.breed);
+
+        Gigi.legs = 4;
+        System.err.println(Gigi.legs);
     }
 }
 
@@ -25,4 +35,12 @@ class Fish extends Animal {
     void swim() {
         System.out.println("swims in water");
     }
+}
+
+class Mammals extends Animal {
+    int legs;
+}
+
+class Dog extends Mammals {
+    String breed;
 }
