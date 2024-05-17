da375e205fcd71581bad88630dad35c241eb5d03
Sumit Pandey <spandey247789@gmail.com>
2024-05-17 09:34:29 +0530

    fibonacci series - recursion

--- recursionAndBackTracking/recursion/FibonacciSeries.java
+++ recursionAndBackTracking/recursion/FibonacciSeries.java
@@ -1,7 +1,20 @@
 package recursionAndBackTracking.recursion;
 
 public class FibonacciSeries {
+    public static int fibonacci(int n) {
+        // Base Case
+        if(n == 0 || n == 1) {
+            return n;
+        }
+
+        int fibNM1 = fibonacci(n - 1);
+        int fibNM2 = fibonacci(n - 2);
+        int fibN = fibNM1 + fibNM2;
+
+        return fibN;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Fibonacci Series");
+        System.out.println(fibonacci(7));
     }
 }
